6c9273da92858422f0588b91b0be734b5e1ce867
jakiichu4 <zellda444@gmail.com>
2025-09-06 00:51:41 +0700

    fix naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import {type ReactNode} from 'react';
 import Table from "@/ui/table/core";
 import useTableStore from "@/bll/store";
-import {useEngine} from "@/bll/engine";
 import type {ITableState} from "@/bll/interface";
-import {compact, flatMapDeep, uniqBy} from "es-toolkit";
+import {compact, flatMapDeep, uniq} from "es-toolkit";
+import {useInterval} from "@siberiacancode/reactuse";
 
 const fiendsItems = [-1, 0, 1]
 const App = (): ReactNode => {
     const {table, flatMap} = useTableStore()
 
-    const fiendNeighbour = (item: ITableState) => {
+
+    const fiendNeighbour = (item: ITableState) => compact(fiendsItems.map(fiend => table?.[item.rowIndex - fiend])
+        .map(col => col?.slice(item.colIndex > 0 ? 0 : item.colIndex, item.colIndex + 2)))
 
 
-        return compact(fiendsItems.map(fiend => table?.[item.rowIndex - fiend]).map(col => {
-            return col?.slice(item.colIndex > 0 ? 0 : item.colIndex, item.colIndex + 2)
-        }))
-    }
+    const uniqCurrentNeighbour = uniq(flatMapDeep(flatMap()
+        .filter(item => item.value)
+        .map(fiendNeighbour), item => [item]))
+
 
     const callbackEngine = () => {
-        const finderNeighbour = flatMapDeep(flatMap().filter(item => item.value).map(fiendNeighbour), item => [item])
-        console.log(uniqBy(finderNeighbour, item => item.id))
+        console.log(uniqCurrentNeighbour)
     }
 
-    const {start, stop} = useEngine(callbackEngine)
+    const {toggle} = useInterval(callbackEngine, 1000)
+
     return (<>
 
-            <button onClick={start}>start</button>
-            <button onClick={stop}>stop</button>
+            <button onClick={toggle}>toggle</button>
             <Table items={table}/>
         </>
     );
